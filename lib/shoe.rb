021546a91c051141086b3b0261ffbca854e3bac8
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-30 00:51:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,10 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand 
   def initialize(brand)
+  end 
+  
+  def cobble 
+    puts "the show has been repaired!"
   end 
 end 
